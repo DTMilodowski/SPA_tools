750732575011e71e6c3add2d6582f88f55203a51
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-09 12:51:06 +0000

    intro to functions

--- CARDAMOM/CARDAMOM_plotlevel_setup.py
+++ CARDAMOM/CARDAMOM_plotlevel_setup.py
@@ -2,8 +2,11 @@
 # 1) Biomass - from GEM census
 # 2) Root stocks - from GEM plots
 # 3) Litter traps - from GEM plots
+# 4) LAI estimates - from  MODIS time series
+# 5) Soil carbon - HWSD in absence of field data
+# 6) Soil respiration? Don't have raw data at present
 
 # These all provide priors into the simulation which in turn help to refine the acceptable parameter space for
-# the various parameters in DALEC.
+# the various parameters in DALEC that will form the starting point for subsequent more detailed SPA modelling.
 
 # CARDAMOM-DALEC is driven with remotely sensed meteorologoical data - from ERA-Interim Reanalysis and TRMM
