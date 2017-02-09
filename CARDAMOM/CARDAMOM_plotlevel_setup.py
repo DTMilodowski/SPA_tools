fa28519349f57d1efdbd46d3b42eee857833e24e
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-09 12:45:47 +0000

    functions to set up plot level CARDAMOM runs

---
+++ CARDAMOM/CARDAMOM_plotlevel_setup.py
@@ -0,0 +1,9 @@
+# This code contains functions to set up a CARDAMOM run for an individual GEM plot, taking in estimates of:
+# 1) Biomass - from GEM census
+# 2) Root stocks - from GEM plots
+# 3) Litter traps - from GEM plots
+
+# These all provide priors into the simulation which in turn help to refine the acceptable parameter space for
+# the various parameters in DALEC.
+
+# CARDAMOM-DALEC is driven with remotely sensed meteorologoical data - from ERA-Interim Reanalysis and TRMM
