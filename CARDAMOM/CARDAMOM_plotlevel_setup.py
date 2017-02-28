0f4bf5c2593d206a8bae39fe7ae482babb47dd5e
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-28 15:43:13 +0000

    emergency reboot of server

--- CARDAMOM/CARDAMOM_plotlevel_setup.py
+++ CARDAMOM/CARDAMOM_plotlevel_setup.py
@@ -22,19 +22,61 @@
 # import some libraries - update as required
 import numpy as np
 import sys
-sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/ERAinterim/")
-sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/UtilityTools/")
-sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/TRMM/")
-sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/gapfilling/")
-
-import weather_generator_BALI as wg
-from statistics_tools import *
-from metdata_processing import *
-import TRMM_processing as TRMM
-
 sys.path.append("/home/dmilodow/DataStore_DTM/BALI/SPA_BALI_data_and_analysis/scripts/field_data/")
 import load_field_data as field
 
 sys.path.append("/home/dmilodow/DataStore_DTM/BALI/SPA_BALI_data_and_analysis/scripts/construct_drivers/")
 import construct_met_drivers as construct_met
 
+#---------------------------------------------------------------------------------------------------------------
+# First of all, define the relevant input files
+# MET DATA
+#    -ERA Interim
+ERA_file = '/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/ERAinterim/BALI_Met/BALI_ERA_Interim_Met.txt'
+#    -TRMM
+ TRMM_file = '/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/TRMM/g4.areaAvgTimeSeries.TRMM_3B42_007_precipitation.20110101-20160429.117E_4N_117E_4N.csv'
+
+# LAI DATA
+#    -MODIS
+
+
+# FIELD DATA
+#    -Plot census data
+#    -Fine roots data
+#    -Litter data
+#    -LiDAR data (for LAI)
+
+
+
+#---------------------------------------------------------------------------------------------------------------
+# Now get some basic parameters for the run
+start_date= '01/01/2011 00:00'
+end_date= '01/01/2016 00:00'
+timestep = 48*30.
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------
+# Process met data
+#   -this function returns a dictionary with time series of meteorological variables to be assimilated into
+#    CARDAMOM
+gapfilled_data = construct_met.EO_metdata(met_station_file,ERA_file,TRMM_file,start_date,end_date)
+
+    start_date_for_cycles = '01/01/2012 00:00'
+    end_date_for_cycles = '01/01/2016 00:00'
+    gapfilled_data_for_cycles = gapfill_metdata(met_station_file,ERA_file,TRMM_file,start_date_for_cycles,end_date_for_cycles)
+    gapfilled_data_ext = extend_metdata(gapfilled_data_for_cycles,2)
+
+    # now concatenate met data
+    MetData = {}
+    MetData['Time']=np.concatenate((gapfilled_data['Time'],gapfilled_data_ext['Time']))
+    MetData['airT']=np.concatenate((gapfilled_data['airT'],gapfilled_data_ext['airT']))
+    MetData['pptn']=np.concatenate((gapfilled_data['pptn'],gapfilled_data_ext['pptn']))
+    MetData['vpd']= np.concatenate((gapfilled_data['vpd'], gapfilled_data_ext['vpd']))
+    MetData['par']= np.concatenate((gapfilled_data['par'], gapfilled_data_ext['par']))
+    MetData['swr']= np.concatenate((gapfilled_data['swr'], gapfilled_data_ext['swr']))
+    MetData['sp']=  np.concatenate((gapfilled_data['sp'],  gapfilled_data_ext['sp']))
+
+    write_metdata_to_SPA_input(SPAMetDir,SPAMetName,MetData,tstep)
