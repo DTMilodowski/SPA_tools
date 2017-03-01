b797bc2dd1b0101f7a9d1fa540bd78516537336c
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-03-01 12:27:22 +0000

    setting up download function

--- CARDAMOM/CARDAMOM_setup_drivers/MODIS_setup.py
+++ CARDAMOM/CARDAMOM_setup_drivers/MODIS_setup.py
@@ -1,3 +1,7 @@
 # This library contains code to download MODIS data and prepare it for assimilation into CARDAMOM
 import numpy as np
 import requests
+
+def download_MODIS_LAI_for_point(lat,long):
+
+    return LAI, LAI_std, LAI_qc
