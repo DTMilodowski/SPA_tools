59b35012d2d518b2e60f88b5487e0392bcda9c1e
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-03-02 11:13:21 +0000

    cleaning up code

--- CARDAMOM/CARDAMOM_setup_drivers/met_setup.py
+++ CARDAMOM/CARDAMOM_setup_drivers/met_setup.py
@@ -1,20 +1,13 @@
 # This set of functions prepares met data for inclusion into DALEC runs
 # Includes ERA interim and TRMM
 import numpy as np
-from matplotlib import pyplot as plt
-import datetime as dt
-from matplotlib import rcParams
-from scipy import signal
 
 import sys
 sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/ERAinterim/")
-sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/UtilityTools/")
 sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/TRMM/")
 sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/gapfilling/")
 
 import weather_generator_BALI as wg
-from statistics_tools import *
-from metdata_processing import *
 import TRMM_processing as TRMM
 
 # generate daily met drivers from ERA-Interim and TRMM
