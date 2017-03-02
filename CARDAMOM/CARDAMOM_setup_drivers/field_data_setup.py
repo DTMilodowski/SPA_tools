412bfc99ed7f62b37057b131d580ce807a1f4650
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-03-02 11:59:36 +0000

    fine root stocks time series

--- CARDAMOM/CARDAMOM_setup_drivers/field_data_setup.py
+++ CARDAMOM/CARDAMOM_setup_drivers/field_data_setup.py
@@ -21,7 +21,6 @@ def get_root_NPP_ts(roots_file,plot):
     rootStocks,rootNPP = field.read_soil_stocks_and_npp(roots_file)
     N_collections = rootNPP[plot]['AccumulationDays'].shape[1]
     collection_dates = []
-    previous_collection_dates = []
     NPP = []
     NPP_std = []
     for i in range(0,N_collections):
@@ -35,6 +34,26 @@ def get_root_NPP_ts(roots_file,plot):
             NPP_std.append(np.std(rootNPP[plot]['FineRootNPP'][jj,i]))
         
     return np.asarray(collection_dates), np.asarray(previous_collection_dates), np.asarray(NPP), np.asarray(NPP_std)
+    
+
+# Get time series of fine root carbon
+def get_Croot_ts(roots_file,plot):
+    rootStocks,rootNPP = field.read_soil_stocks_and_npp(roots_file)
+    N_collections = rootStocks[plot]['FineRootStocks'].shape[1]
+    collection_dates = []
+    previous_collection_dates = []
+    Croot = []
+    Croot_std = []
+    for i in range(0,N_collections):
+        # check that there is a collection this year
+        if np.max(rootStocks[plot]['CollectionDate'][:,i])>np.datetime64('2000-01-01'):
+            collection_dates.append(np.max(rootStocks[plot]['CollectionDate'][:,i]))
+            # avoid nodata for missing/destroyed traps
+            jj = np.isfinite(rootStocks[plot]['FineRootStocks'][:,i])
+            Croot.append(np.mean(rootStocks[plot]['FineRootStocks'][jj,i]))
+            Croot_std.append(np.std(rootStocks[plot]['FineRootStocks'][jj,i]))
+        
+    return np.asarray(collection_dates), np.asarray(Croot), np.asarray(Croot_std)
     
 # Get time series of litter fall
 def get_litterfall_ts(litter_file,plot):
