34cbcb0a7d4e19765faf06a5021b6a7429979e8a
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-03-22 16:41:03 +0000

    copied across duplicated code from existing scripts

--- CARDAMOM/CARDAMOM_write_drivers_and_priors_to_file.py
+++ CARDAMOM/CARDAMOM_write_drivers_and_priors_to_file.py
@@ -18,3 +18,90 @@ sys.path.append('/home/dmilodow/DataStore_DTM/BALI/SPA_BALI_data_and_analysis/sc
 import met_setup as met
 import MODIS_setup as MODIS
 import field_data_setup as field
+
+# First of all, define the relevant input files
+# MET DATA
+#    -ERA Interim
+ERA_file = '/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/ERAinterim/BALI_Met/BALI_ERA_Interim_Met.txt'
+#    -TRMM
+TRMM_file = '/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/TRMM/g4.areaAvgTimeSeries.TRMM_3B42_007_precipitation.20110101-20160429.117E_4N_117E_4N.csv'
+
+# LAI DATA
+#    -MODIS
+MODIS_file = '/home/dmilodow/DataStore_DTM/BALI/MODIS/BALI-GEM-MODIS-MOD15A2H-006-results.csv'
+
+# FIELD DATA
+#    -Plot census data
+#    -Fine roots data
+#    -Litter data
+#    -LiDAR data (for LAI)
+census_file = '/home/dmilodow/DataStore_DTM/BALI/BALI_Cplot_data/SAFE_CarbonPlots_TreeCensus.csv'
+roots_file = '/home/dmilodow/DataStore_DTM/BALI/BALI_Cplot_data/SAFE_CarbonPlots_FineRoots_Stock_NPP_RawData.csv'
+litter_file = '/home/dmilodow/DataStore_DTM/BALI/BALI_Cplot_data/SAFE_CarbonPlots_Litterfall_RawData.csv'
+
+#---------------------------------------------------------------------------------------------------------------
+# Now get some basic parameters for the run
+start_date= '01/01/2011'
+end_date= '01/03/2016'
+plot = 'Belian'
+
+# Initiate some arrays to host time series
+d,m,y = start_date.split('/')
+start = np.datetime64(y+'-'+m+'-'+d,'D')
+d,m,y = end_date.split('/')
+end = np.datetime64(y+'-'+m+'-'+d,'D')
+date = np.arange(start,end, dtype = 'datetime64[D]')
+
+N_t = date.size
+mn2t_in = np.zeros(N_t)
+mx2t_in = np.zeros(N_t)
+vpd_in = np.zeros(N_t)
+ssrd_in = np.zeros(N_t)
+pptn_in = np.zeros(N_t)
+
+Cwood_in = np.zeros(N_t)
+Croot_in = np.zeros(N_t)
+Litter_in = np.zeros(N_t)
+
+LAI_in = np.zeros(N_t)
+LAI_std_in = np.zeros(N_t)
+
+#---------------------------------------------------------------------------------------------------------------
+# Process met data
+#   -this function returns a dictionary with time series of meteorological variables to be assimilated into
+#    CARDAMOM
+#   - Variable keys: Time, airT, pptn, vpd, par, swr, sp
+met_dates, mn2t, mx2t, vpd, ssrd, TRMM_dates, pptn= met.generate_daily_met_drivers_ERAinterim_TRMM(ERA_file, TRMM_file, start, end)
+N_m = met_dates.size
+for dd in range(0,N_m):
+    mn2t_in[date == met_dates[dd]] = mn2t[dd]
+    mx2t_in[date == met_dates[dd]] = mx2t[dd]
+    ssrd_in[date == met_dates[dd]] = ssrd[dd]
+    vpd_in[date == met_dates[dd]] = vpd[dd]
+N_trmm = TRMM_dates.size
+for dd in range(0,N_trmm):
+    pptn_in[date == TRMM_dates[dd]] = pptn[dd]
+#---------------------------------------------------------------------------------------------------------------
+# Process LAI data
+#   -this function returns a dictionary with time series of LAI from MODIS to drive DALEC.
+LAI_dict = MODIS.load_point_MODIS_LAI_time_series_from_file(MODIS_file)
+LAI = LAI_dict[plot]
+N_LAI = LAI['date'].size
+for dd in range(0,N_LAI):
+    LAI_in[date == LAI['date'][dd]] = LAI['LAI'][dd]
+    LAI_std_in[date == LAI['date'][dd]] = LAI['LAI_std'][dd]
+#---------------------------------------------------------------------------------------------------------------
+# Process field data
+census_date, Cwood = field.get_Cwood_ts(census_file,plot)
+N_c = Cwood.size
+for dd in range(0,N_c):
+    Cwood_in[date == census_date[dd]] = Cwood[dd]
+
+root_stock_date, Croot, Croot_std = field.get_Croot_ts(roots_file,plot)
+N_r = Croot.size
+for dd in range(0,N_r):
+    Croot_in[date == root_stock_date[dd]] = Croot[dd]
+
+
+litter_collection_date, litter_previous_collection_date, litter_flux, litter_std = field.get_litterfall_ts(litter_file,plot)
+# work out what to do with litter data
