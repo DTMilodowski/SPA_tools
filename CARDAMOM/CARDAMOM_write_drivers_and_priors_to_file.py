e00715edd4259273455be0b1bfc6367ae18816f0
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-03-24 14:55:34 +0000

    outputs 21 day rolling averages for met data

--- CARDAMOM/CARDAMOM_write_drivers_and_priors_to_file.py
+++ CARDAMOM/CARDAMOM_write_drivers_and_priors_to_file.py
@@ -60,6 +60,11 @@ mx2t_in = np.zeros(N_t)-9999.
 vpd_in = np.zeros(N_t)-9999.
 ssrd_in = np.zeros(N_t)-9999.
 pptn_in = np.zeros(N_t)-9999.
+mn2t21_in = np.zeros(N_t)-9999.
+mx2t21_in = np.zeros(N_t)-9999.
+vpd21_in = np.zeros(N_t)-9999.
+ssrd21_in = np.zeros(N_t)-9999.
+pptn21_in = np.zeros(N_t)-9999.
 
 #---------------------------------------------------------------------------------------------------------------
 # Process met data
@@ -76,6 +81,18 @@ for dd in range(0,N_m):
 N_trmm = TRMM_dates.size
 for dd in range(0,N_trmm):
     pptn_in[date == TRMM_dates[dd]] = pptn[dd]
+
+# also get 21 day rolling average (retro-looking)
+mn2t21,mx2t21,ssrd21,vpd21,pptn21 = met.retro_rolling_average_met_data(mn2t,mx2t,ssrd,vpd,pptn)
+for dd in range(0,N_m):
+    mn2t21_in[date == met_dates[dd]] = mn2t21[dd]
+    mx2t21_in[date == met_dates[dd]] = mx2t21[dd]
+    ssrd21_in[date == met_dates[dd]] = ssrd21[dd]
+    vpd21_in[date == met_dates[dd]] = vpd21[dd]
+for dd in range(0,N_trmm):
+    pptn21_in[date == TRMM_dates[dd]] = pptn21[dd]
+
+
 #---------------------------------------------------------------------------------------------------------------
 # Process field data
 
@@ -129,6 +146,11 @@ for pp in range(0,len(plot)):
     ssrd_in[np.isnan(ssrd_in)]=-9999.
     vpd_in[np.isnan(vpd_in)]=-9999.
     pptn_in[np.isnan(pptn_in)]=-9999.
+    mx2t21_in[np.isnan(mx2t_in)]=-9999.
+    mn2t21_in[np.isnan(mn2t_in)]=-9999.
+    ssrd21_in[np.isnan(ssrd_in)]=-9999.
+    vpd21_in[np.isnan(vpd_in)]=-9999.
+    pptn21_in[np.isnan(pptn_in)]=-9999.
 
 
     # write output to file
@@ -138,10 +160,10 @@ for pp in range(0,len(plot)):
     outfile_priors = "CARDAMOM_param_priors_"+plot[pp]+".csv"
     out_priors = open(outfile_priors,'w')
 
-    out_drivers.write('timestep_days, date, mn2t, mx2t, vpd, ssrd, pptn, LAI\n')
+    out_drivers.write('timestep_days, date, mn2t, mx2t, vpd, ssrd, pptn, mn2t_21d, mx2t_21d, vpd_21d, ssrd_21d, pptn_21d, LAI\n')
     out_priors.write('timestep_days, date, Cwood, Croot, Csoil, LitterFlux, LitterFluxStd\n')
     for tt in range(0,N_t):
-        out_drivers.write(str(tt) + ',' + str(date[tt]) + ', ' + str(mn2t_in[tt]) + ',' + str(mx2t_in[tt]) + ',' + str(vpd_in[tt]) + ',' + str(ssrd[tt]) + ',' + str(pptn[tt]) + ',' +  str(LAI[pp])  + '\n')
+        out_drivers.write(str(tt) + ',' + str(date[tt]) + ', ' + str(mn2t_in[tt]) + ',' + str(mx2t_in[tt]) + ',' + str(vpd_in[tt]) + ',' + str(ssrd_in[tt]) + ',' + str(pptn_in[tt]) + ',' + str(mn2t21_in[tt]) + ',' + str(mx2t21_in[tt]) + ',' + str(vpd21_in[tt]) + ',' + str(ssrd21_in[tt]) + ',' + str(pptn21_in[tt]) + ',' +    str(LAI[pp])  + '\n')
         out_priors.write(str(tt) + ',' + str(date[tt]) + ', ' + str(Cwood_in[tt]) + ',' + str(Croot_in[tt]) + ',' + str(Csoil_in[tt]) + ',' + str(Litter_in[tt]) + ',' + str(Litter_std_in[tt]) + '\n')
     out_drivers.close()
     out_priors.close()
