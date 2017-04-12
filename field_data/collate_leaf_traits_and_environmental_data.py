0fad9c67aba1f7de83c82e1bb0924cd5a68af137
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-12 15:58:26 +0100

    import and clip lidar data for collation with traits data

--- field_data/collate_leaf_traits_and_environmental_data.py
+++ field_data/collate_leaf_traits_and_environmental_data.py
@@ -30,16 +30,28 @@ census_file = '/home/dmilodow/DataStore_DTM/BALI/BALI_Cplot_data/SAFE_CarbonPlot
 field_file = '/home/dmilodow/DataStore_DTM/BALI/LiDAR/Data/Local/SAFE_DANUM_carbonplots_FieldMapcensus2016.csv'
 light_file = '/exports/csce/datastore/geos/users/dmilodow/BALI/LiDAR/src/output/BALI_subplot_lighttransmittance.npz'
 subplot_coordinate_file = '/home/dmilodow/DataStore_DTM/BALI/LiDAR/src/BALI_subplot_coordinates_corrected.csv' #check
+las_file = ''
 
-
+# get LiDAR data and sort into plots
+lidar_pts = lidar.load_lidar_data(las_file)
+plot_pts = {}
 
 # get lower left hand corner of plots
 subplot_polygons, subplot_labels = aux.load_boundaries(subplot_coordinate_file)
 plot_origin = {}
+buffer_width = 5.
 for i in range(0,len(subplot_polygons.keys())):
     plot = subplot_polygons.keys()[i]
     plot_origin[plot] = subplot_polygons[plot][0,0,:]
 
+    # clip LiDAR point cloud to plot level (this makes subsequent processing much faster)
+    n_coord_pairs = subplot_polygons[Plot_name].shape[0]*subplot_polygons[Plot_name].shape[1]
+    coord_pairs = subplot_polygons[Plot_name].reshape(n_coord_pairs,2)
+    bbox_polygon = aux.get_bounding_box_with_buffer(coord_pairs,buffer_width)
+    plot_pts[plot] = lidar.filter_lidar_data_by_polygon(lidar_pts,bbox_polygon)
+
+# clear up some memory
+lidar_pts = None
 
 
 # Load in the traits
@@ -55,8 +67,6 @@ Narea[Narea>4.0]=np.nan
 LMA_C = LMA*C/100. # convert to g(C) m^-2
 LMA_C[LMA_C>100.]=np.nan
 
-
-#=============================================
 # Loop through branches pulling out plot codes
 N_branches = branch.size
 plot_list = []
@@ -95,29 +105,58 @@ field_data = inventory.load_crown_survey_data(field_file)
 
 
 subplot = np.zeros(N_branches)*np.nan
+light_availability = np.zeros(N_branches)*np.nan
+tree_centric = np.zeros(N_branches) # a flag to state whether or not we have tree-centric LAD to estimate light environment
 
 plots = np.unique(plot)
+radius = 5
+layer_thickness = 1
+max_height = 80
+k=0.5
 for i in range(0,N_branches):
     tree_index = np.all((census_plot==plot[i], np.any((tree_tag == tree[i], alt_tag == tree[i]),axis=0)),axis=0)
     tree_index_alt = np.all((field_data['plot']==plot[i], field_data['tag'] == tree[i]),axis=0)
-    
+
+    # first case scenario - we have located tree in canopy map
     if tree_index_alt.sum() == 1:
         subplot[i] = field_data['subplot'][tree_index_alt]
+        tree_xy = np.asarray([field_data['Xfield'][tree_index_alt]+ plot_origin[plot[i]][0],field_data['Yfield'][tree_index_alt]+ plot_origin[plot[i]][1]])
+        tree_pts = filter_lidar_data_by_polygon(plot_pts[plot[i]],tree_xy,radius)
+        heights,first_return_profile,n_ground_returns = LAD1.bin_returns(tree_pts, max_height,layer_thickness)
+        LAD_tree = LAD.estimate_LAD_MacArtherHorn(first_return_profile, n_ground_returns, layer_thickness, 1.)
+        I_tree=clim.estimate_canopy_light_transmittance(LAD_tree,heights,k)
+        ht_index = int(LeafHeight[i])
+        light_availability[i]=I_tree[ht_index]
+        tree_centric[i] = 1
+
+    # otherwise, need to use census data, which only gives subplot level data
     elif tree_index.sum() == 1:
         #print "found tree :-)"
         subplot[i] = census_subplot[tree_index]
+        subplot_poly = subplot_polygons[Plot_name][subplot_labels[plot[i]] == subplot[i],:,:]
+        sp_pts = lidar.filter_lidar_data_by_polygon(plot_pts[plot[i]],subplot_poly)
+        heights,first_return_profile,n_ground_returns = LAD1.bin_returns(tree_pts, max_height,layer_thickness)
+        LAD_subplot = LAD.estimate_LAD_MacArtherHorn(first_return_profile, n_ground_returns, layer_thickness, 1.)
+        I_subplot=clim.estimate_canopy_light_transmittance(LAD_subplot,heights,k)
+        ht_index = int(LeafHeight[i])
+        light_availability[i]=I_subplot[ht_index]
+
     elif np.all((tree_index.sum()==0,tree_index_alt.sum()==0)):
         print "cannot find tree from traits record in the census data"
         print "\t tree: ", tree[i], "; plot: ", plot[i]
         subplot[i] = np.nan
+        light_availability[i]=np.nan
     else:
         print "we have a problem - more than one tree in this plot has the same tag "
         subplot[i] = np.nan
 
-# load in light transmittance profiles
+
+
+
+
+# load in light transmittance profiles (subplot level, not tree centric)
 I = np.load(light_file)
 
-light_availability = np.zeros(N_branches)*np.nan
 #currently I has vertical resolution of 1 m.  Bin 0 -> 0-1 m.  Therefore take floor of leaf height to find index required
 for i in range(0,N_branches):
     if np.all((np.isfinite(LeafHeight[i]), np.isfinite(subplot[i]))):
