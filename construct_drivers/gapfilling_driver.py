d3701e00da039ef40814fd42da838fb834d89aea
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-18 15:32:52 +0100

    test driver for gapfilling routine

---
+++ construct_drivers/gapfilling_driver.py
@@ -0,0 +1,16 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import datetime as dt
+from matplotlib import rcParams
+from scipy import signal
+
+import sys
+sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/ERAinterim/")
+sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/UtilityTools/")
+sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/TRMM/")
+sys.path.append("/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/gapfilling/")
+
+import weather_generator_BALI as wg
+from statistics_tools import *
+import metdata_processing as met
+import TRMM_processing as TRMM
