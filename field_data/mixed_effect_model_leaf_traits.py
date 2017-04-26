7be72410b987bd9b4b0a3b3c20494b3b7d5008f2
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-26 16:49:32 +0100

    swtiching to pandas

--- field_data/mixed_effect_model_leaf_traits.py
+++ field_data/mixed_effect_model_leaf_traits.py
@@ -2,6 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 import sys
 
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+import pandas as pd
+
 
 from matplotlib import rcParams
 # Set up some basiic parameters for the plots
@@ -14,6 +19,12 @@ axis_size = rcParams['font.size']+2
 # traits file
 leaf_traits_file = 'BALI_leaf_traits.csv'
 
-datatype={'names':('plot','subplot','forest_type','branch','shade_tag','spp','genus','leaf_height','light_availability','leaf_thickness','leaf_area','LMA','C%','Carea','N%','Narea','Vcmax','Rd'),'formats':('S16','i8','S16','S16','i8','S32','S32','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16')}
-data = np.genfromtxt(chem_file,skiprows=1,delimiter=',',dtype=datatype)
+#datatype={'names':('plot','subplot','forest_type','branch','shade_tag','spp','genus','leaf_height','light_availability','leaf_thickness','leaf_area','LMA','C%','Carea','N%','Narea','Vcmax','Rd'),'formats':('S16','i8','S16','S16','i8','S32','S32','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16')}
+#data = np.genfromtxt(chem_file,skiprows=1,delimiter=',',dtype=datatype)
+
+data = pd.read_csv(leaf_traits_file)
+
+md = smf.mixedlm("Weight ~ Time", data, groups=data["Pig"])
+mdf = md.fit()
+
 
