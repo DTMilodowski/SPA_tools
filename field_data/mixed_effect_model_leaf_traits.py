0feb5e1acbf4c4fdaa323923ff9c22e1eb96e6b2
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-26 16:35:25 +0100

    creating new script to undertake mixed effect modelling of leaf traits

---
+++ field_data/mixed_effect_model_leaf_traits.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+
+
+from matplotlib import rcParams
+# Set up some basiic parameters for the plots
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['arial']
+rcParams['font.size'] = 8
+rcParams['legend.numpoints'] = 1
+axis_size = rcParams['font.size']+2
+
+
