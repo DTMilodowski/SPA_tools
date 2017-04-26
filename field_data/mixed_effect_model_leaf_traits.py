a230db59619dc2611e4bee52811d58d5a8cd95c2
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-26 16:40:40 +0100

    reading in compiled leaf traits from file

--- field_data/mixed_effect_model_leaf_traits.py
+++ field_data/mixed_effect_model_leaf_traits.py
@@ -11,4 +11,9 @@ rcParams['font.size'] = 8
 rcParams['legend.numpoints'] = 1
 axis_size = rcParams['font.size']+2
 
+# traits file
+leaf_traits_file = 'BALI_leaf_traits.csv'
+
+datatype={'names':('plot','subplot','forest_type','branch','shade_tag','spp','genus','leaf_height','light_availability','leaf_thickness','leaf_area','LMA','C%','Carea','N%','Narea','Vcmax','Rd'),'formats':('S16','i8','S16','S16','i8','S32','S32','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16','f16')}
+data = np.genfromtxt(chem_file,skiprows=1,delimiter=',',dtype=datatype)
 
