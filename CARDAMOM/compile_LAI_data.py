ca5430961435e6e552c2decaa47f2ceab2082658
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-05-08 13:24:48 +0100

    new script collating and comparing LAI data and other related datasets

---
+++ CARDAMOM/compile_LAI_data.py
@@ -0,0 +1,11 @@
+# This function essentially compiles together all of the available LAI data for the BALI
+# project for ease of comparison and to facilitate their incorporation into the numerical
+# modelling framework.  Sources of LAI data (and other relevant proxies:
+# (1)  hemispherical photographs, processed with Hemisfer ->  LAI estimates for each plot
+#      have been made at multiple points in time, providing a sparse time series of LAI
+# (2)  LiDAR, processed either following Stark et al. (2012) or an alternative approach,
+#      (Detto et al., 2015, improved by D T Milodowski et al., in prep).
+# (3)  MODIS: 250 m resolution MODIS LAI from the latest MODIS LAI product: MOD15A2H
+# (4)  Litter flux estimates
+# (5)  Canopy volume estimates.
+#----------------------------------------------------------------------------------------
